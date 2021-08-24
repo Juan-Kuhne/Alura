70cd04997235ac8e7f4636568f158227ae497d2c
Juan-Kuhne <61292150+Juan-Kuhne@users.noreply.github.com>
2021-08-24 09:21:24 -0300

    Orientação a objetos parte 2 - Aula 5

--- Formação Front-End/Javascript/Orientação a Objetos/SistemaAutenticacao.js
+++ Formação Front-End/Javascript/Orientação a Objetos/SistemaAutenticacao.js
@@ -5,6 +5,14 @@ Ser autenticável significa ter o método 'senha'
 export class SistemaAtuenticacao {
 
     static login(autenticavel, senha) {
-        return autenticavel.autenticar(senha)
+        if(SistemaAtuenticacao.ehAutenticavel(autenticavel)) {
+            return autenticavel.autenticar(senha)
+        }
+
+        return false
+    }
+
+    static ehAutenticavel(autenticavel) {
+        return 'autenticar' in autenticavel && autenticavel.autenticar instanceof Function
     }
 }
