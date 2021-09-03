a168830d0ce1d808ce466328e7fd078db2038e88
Juan Kuhne <juankuhne@hotmail.com>
2021-09-03 16:04:37 -0300

    Javascript DOM - Aula 5.3

---
+++ Formação Front-End/Sites Interativos/Javascript DOM/components/concluiTarefa.js
@@ -0,0 +1,19 @@
+export default BotaoConclui
+
+const BotaoConclui = () => {
+    const botaoConclui = document.createElement('button')
+    
+    botaoConclui.classList.add('check-button')
+    botaoConclui.innerText = 'Concluir'
+    botaoConclui.addEventListener('click', concluirTarefa)
+
+    return botaoConclui
+}
+
+const concluirTarefa = (evento) => {
+    const botaoConclui = evento.target
+
+    const tarefaCompleta = botaoConclui.parentElement
+
+    tarefaCompleta.classList.toggle('done')
+}
