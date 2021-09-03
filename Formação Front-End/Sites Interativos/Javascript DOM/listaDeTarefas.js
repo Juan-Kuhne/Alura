40812c7368a5d1fa437ac09c77feccf275fa9585
Juan Kuhne <juankuhne@hotmail.com>
2021-09-03 13:23:41 -0300

    Javascript DOM - Aula 3

--- Formação Front-End/Sites Interativos/Javascript DOM/listaDeTarefas.js
+++ Formação Front-End/Sites Interativos/Javascript DOM/listaDeTarefas.js
@@ -1,10 +1,18 @@
 const criarTarefa = (evento) => {
 
     evento.preventDefault()
-    
+
+    const lista = document.querySelector('[data-list]')
     const input = document.querySelector('[data-form-input]')
     valor = input.value
-    console.log(valor)
+
+    const conteudo = `<p class="content">${valor}</p>`
+    const tarefa = document.createElement('li')
+    tarefa.classList.add('task')
+
+    tarefa.innerHTML = conteudo
+    lista.appendChild(tarefa)
+
     input.value = ''
 }
 
