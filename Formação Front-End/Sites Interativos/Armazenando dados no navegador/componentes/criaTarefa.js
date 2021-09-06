54fc675bef44e064690dba86b6c2bb16f62676b4
Juan Kuhne <juankuhne@hotmail.com>
2021-09-06 14:53:53 -0300

    Armazenando dados no navegador - Aula 4.3

--- Formação Front-End/Sites Interativos/Armazenando dados no navegador/componentes/criaTarefa.js
+++ Formação Front-End/Sites Interativos/Armazenando dados no navegador/componentes/criaTarefa.js
@@ -1,42 +1,45 @@
+import { carregaTarefa } from './carregaTarefa.js'
 import BotaoConclui from './concluiTarefa.js'
 import BotaoDeleta from './deletaTarefa.js'
+ 
 
 export const handleNovoItem = (evento) => {
     evento.preventDefault()
-    const tarefas = JSON.parse(localStorage.getItem('tarefas')) || []
-    const lista = document.querySelector('[data-list]')
+    const tarefas = JSON.parse(localStorage.getItem('tarefas'))||[]
     const input = document.querySelector('[data-form-input]')
-    const calendario = document.querySelector('[data-form-date]')
-    
     const valor = input.value
-    const data = moment(calendario.value).format('DD/MM/YYYY')
 
-    const dados = {
-        valor, 
-        data
+    const calendario = document.querySelector('[data-form-date]')
+    const data = moment(calendario.value)
+
+    const dataFormatada = data.format('DD/MM/YYYY')
+
+    const dados = { 
+        valor,
+        dataFormatada
     }
 
-    const tarefa = criarTarefa(dados)
+    const tarefasAtualizadas = [...tarefas, dados]
 
-    const tarefasAtualizado = [...tarefas, dados]
+    localStorage.setItem('tarefas', JSON.stringify(tarefasAtualizadas))
 
-    localStorage.setItem("tarefas", JSON.stringify(tarefasAtualizado))
-
-    lista.appendChild(tarefa)
     input.value = " "
 
+    carregaTarefa()
+    
 }
 
-export const criarTarefa = ({valor, data}) => {
+export const Tarefa = ({ valor, dataFormatada }) => {
 
     const tarefa = document.createElement('li')
     tarefa.classList.add('task')
-    const conteudo = `<p class="content">${data} - ${valor}</p> `
+    const conteudo = `<p class="content">${dataFormatada} * ${valor}</p>`
 
     tarefa.innerHTML = conteudo
 
     tarefa.appendChild(BotaoConclui())
     tarefa.appendChild(BotaoDeleta())
-
+   
     return tarefa
+
 }
