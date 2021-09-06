efdc033ede3ac52336f633e793f224dc319a8ec4
Juan Kuhne <juankuhne@hotmail.com>
2021-09-06 10:37:34 -0300

    Armazenando dados no navegador - Aula 1

--- Formação Front-End/Sites Interativos/Armazenando dados no navegador/main.js
+++ Formação Front-End/Sites Interativos/Armazenando dados no navegador/main.js
@@ -1,27 +1,41 @@
 import BotaoConclui from './componentes/concluiTarefa.js'
 import BotaoDeleta from './componentes/deletaTarefa.js'
  
-    const criarTarefa = (evento) => {
-
+const handleNovoItem = (evento) => {
     evento.preventDefault()
-
     const lista = document.querySelector('[data-list]')
     const input = document.querySelector('[data-form-input]')
+    const calendario = document.querySelector('[data-form-date]')
+    
     const valor = input.value
+    const data = moment(calendario.value).format('DD/MM/YYYY')
 
-    const tarefa = document.createElement('li')
-    tarefa.classList.add('task')
-    const conteudo = `<p class="content">${valor}</p>`
+    const dados = {
+        valor, 
+        data
+    }
 
-    tarefa.innerHTML = conteudo
+    const tarefa = criarTarefa(dados)
 
-    tarefa.appendChild(BotaoConclui())
-    tarefa.appendChild(BotaoDeleta())
     lista.appendChild(tarefa)
     input.value = " "
 
 }
 
+const criarTarefa = ({valor, data}) => {
+
+    const tarefa = document.createElement('li')
+    tarefa.classList.add('task')
+    const conteudo = `<p class="content">${data} - ${valor}</p> `
+
+    tarefa.innerHTML = conteudo
+
+    tarefa.appendChild(BotaoConclui())
+    tarefa.appendChild(BotaoDeleta())
+
+    return tarefa
+}
+
 const novaTarefa = document.querySelector('[data-form-button]')
 
-novaTarefa.addEventListener('click', criarTarefa)
+novaTarefa.addEventListener('click', handleNovoItem)
