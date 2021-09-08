30463e1325eae373aaf378077f95a0652256097a
Juan-Kuhne <61292150+Juan-Kuhne@users.noreply.github.com>
2021-09-08 09:57:50 -0300

    Armazenando dados no navegador - Aula 4

--- Formação Front-End/Sites Interativos/Armazenando dados no navegador/componentes/carregaTarefa.js
+++ Formação Front-End/Sites Interativos/Armazenando dados no navegador/componentes/carregaTarefa.js
@@ -1,13 +1,16 @@
 import { criaData } from "./criaData.js"
+import { removeDatasRepetidas, ordenaDatas } from "../service/data.js"
 
-export const carregaTarefa = () => { 
+export const carregaTarefa = () => {
     const lista = document.querySelector('[data-list]')
 
     const tarefasCadastradas = JSON.parse(localStorage.getItem('tarefas')) || []
 
     lista.innerHTML = " "
-    tarefasCadastradas.forEach((tarefa) => {
-        const dia = moment(tarefa.dataFormatada, 'DD/MM/YYYY')
+    const datasUnicas = removeDatasRepetidas(tarefasCadastradas)
+    ordenaDatas(datasUnicas)
+
+    datasUnicas.forEach((dia) => {
         lista.appendChild(criaData(dia))
     })
 }
