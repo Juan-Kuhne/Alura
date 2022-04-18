f2f55a77b844a4b096316665aaff30424bd27ad5
Lucris <juankuhne@hotmail.com>
2022-04-18 13:31:02 -0300

    C# parte 1 (End)

---
+++ Formação C# e orientação a objetos/c# parte 1/AprendendoCSharp/P10-CalculaPoupanca/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P10_CalculaPoupanca
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando projeto 10");
+
+            double valorInvestido = 1000;
+            int contadorMes = 1;
+
+            // 0.36% = 0.0036
+            //valorInvestido = valorInvestido + valorInvestido * 0.0036;
+            //Console.WriteLine("Após 1 mês você terá R$" + valorInvestido);
+            //valorInvestido = valorInvestido + valorInvestido * 0.0036;
+            //Console.WriteLine("Após 2 mês você terá R$" + valorInvestido);
+
+            while (contadorMes <= 12)
+            {
+                valorInvestido += valorInvestido * 0.0036;
+                Console.WriteLine("Após " + contadorMes + " mês você terá R$" + valorInvestido);
+                contadorMes++;
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
