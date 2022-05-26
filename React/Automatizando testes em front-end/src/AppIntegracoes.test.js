75bc5844e1a621e6b2368a85aef40110d09c84d4
Lucris <juankuhne@hotmail.com>
2022-05-26 15:03:33 -0300

    Testando código assíncrono

--- React/Automatizando testes em front-end/src/AppIntegracoes.test.js
+++ React/Automatizando testes em front-end/src/AppIntegracoes.test.js
@@ -7,17 +7,17 @@ import api from "./api";
 jest.mock("./api");
 
 describe("Requisições para API", () => {
-  it("Exibir lista de transações através da API", () => {
+  it("Exibir lista de transações através da API", async () => {
     api.listaTransacoes.mockResolvedValue([
       {
-        valor: 10,
+        valor: "10",
         transacao: "saque",
         data: "10/08/2020",
         id: 1,
       },
       {
         transacao: "deposito",
-        valor: 20,
+        valor: "20",
         data: "26/09/2020",
         id: 2,
       },
@@ -25,7 +25,7 @@ describe("Requisições para API", () => {
 
     render(<App />);
 
-    expect(screen.findByText("saque")).toBeInTheDocument();
+    expect(await screen.findByText("saque")).toBeInTheDocument();
 
     expect(screen.getByTestId("transacoes").children.length).toBe(2);
   });
