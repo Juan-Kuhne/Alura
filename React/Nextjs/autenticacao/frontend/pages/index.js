dde2b260ecaf4e14ecdc346f78be1b005b961e5d
Juan Kuhne <juan.kuhne@certisign.com.br>
2022-06-17 11:05:00 -0300

    login na prática

--- React/Nextjs/autenticacao/frontend/pages/index.js
+++ React/Nextjs/autenticacao/frontend/pages/index.js
@@ -1,22 +1,66 @@
+import React from "react";
+import { useRouter } from "next/router";
+import { authService } from "../src/services/auth/authService";
+
 export default function HomeScreen() {
+  const router = useRouter();
+  const [values, setValues] = React.useState({
+    usuario: "omariosouto",
+    senha: "safepassword",
+  });
+
+  function handleChange(event) {
+    const fieldValue = event.target.value;
+    const fieldName = event.target.name;
+    setValues((currentValues) => {
+      return {
+        ...currentValues,
+        [fieldName]: fieldValue,
+      };
+    });
+  }
+
   return (
     <div>
       <h1>Login</h1>
-      <form>
+      <form
+        onSubmit={(event) => {
+          // onSubmit -> Controller (pega dados do usuario e passa para um serviço)
+          // authService -> Serviço
+          event.preventDefault();
+          authService
+            .login({
+              username: values.usuario,
+              password: values.senha,
+            })
+            .then(() => {
+              // router.push("/auth-page-static");
+              router.push("/auth-page-ssr");
+            })
+            .catch(() => {
+              alert("Usuário ou senha inválidos ...");
+            });
+        }}
+      >
         <input
-          placeholder="Usuário" name="usuario"
-          defaultValue="omariosouto"
+          placeholder="Usuário"
+          name="usuario"
+          value={values.usuario}
+          onChange={handleChange}
         />
         <input
-          placeholder="Senha" name="senha" type="password"
-          defaultValue="safepassword"
+          placeholder="Senha"
+          name="senha"
+          type="password"
+          value={values.senha}
+          onChange={handleChange}
         />
         <div>
-          <button>
-            Entrar
-          </button>
+          <button>Entrar</button>
         </div>
       </form>
+
+      <pre>{JSON.stringify(values, null, 2)}</pre>
     </div>
   );
 }
