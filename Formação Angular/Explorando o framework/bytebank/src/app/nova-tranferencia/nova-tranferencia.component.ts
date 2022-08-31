b6a41a78dc70c0158160bbf0b08e7a0b9c3ff665
Juan_Kuhne <juan.kuhne@certisign.com.br>
2022-08-31 09:59:15 -0300

    Criando o primeiro componente

---
+++ Formação Angular/Explorando o framework/bytebank/src/app/nova-tranferencia/nova-tranferencia.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'nova-tranferencia',
+  templateUrl: './nova-tranferencia.component.html',
+  styleUrls: ['nova-tranferencia.component.scss'],
+})
+export class NovaTransferenciaComponent {}
