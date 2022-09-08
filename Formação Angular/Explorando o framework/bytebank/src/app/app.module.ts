2e2c866173979debb33d0edd9b2c86fa1ba3e431
Juan_Kuhne <juan.kuhne@certisign.com.br>
2022-09-08 13:17:45 -0300

    Melhorando a visualização #1

--- Formação Angular/Explorando o framework/bytebank/src/app/app.module.ts
+++ Formação Angular/Explorando o framework/bytebank/src/app/app.module.ts
@@ -1,15 +1,22 @@
 import { NovaTransferenciaComponent } from './nova-tranferencia/nova-tranferencia.component';
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID, DEFAULT_CURRENCY_CODE } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { ExtratoComponent } from './extrato/extrato.component';
+import { registerLocaleData } from '@angular/common';
+import localePt from '@angular/common/locales/pt';
+
+registerLocaleData(localePt, 'pt');
 
 @NgModule({
   declarations: [AppComponent, NovaTransferenciaComponent, ExtratoComponent],
   imports: [BrowserModule, FormsModule],
-  providers: [],
+  providers: [
+    { provide: LOCALE_ID, useValue: 'pt' },
+    { provide: DEFAULT_CURRENCY_CODE, useValue: 'BRL' },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
