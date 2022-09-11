0c9012df4659ea26ab68e57db0493d5a3862eb17
Lucris <juankuhne@hotmail.com>
2022-09-11 14:29:38 -0300

    Apresentando o módulo Router

--- Formação Angular/Explorando o framework/bytebank/src/app/app.module.ts
+++ Formação Angular/Explorando o framework/bytebank/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { AppRoutingModule } from './app-routing.module';
 import { NovaTransferenciaComponent } from './nova-tranferencia/nova-tranferencia.component';
 import { NgModule, LOCALE_ID, DEFAULT_CURRENCY_CODE } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
@@ -13,7 +14,7 @@ registerLocaleData(localePt, 'pt');
 
 @NgModule({
   declarations: [AppComponent, NovaTransferenciaComponent, ExtratoComponent],
-  imports: [BrowserModule, FormsModule, HttpClientModule],
+  imports: [BrowserModule, FormsModule, HttpClientModule, AppRoutingModule],
   providers: [
     { provide: LOCALE_ID, useValue: 'pt' },
     { provide: DEFAULT_CURRENCY_CODE, useValue: 'BRL' },
