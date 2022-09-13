0f59698303f814175053be195047ced6cfd29ba7
Juan_Kuhne <juan.kuhne@certisign.com.br>
2022-09-13 08:25:04 -0300

    Formulário Template-Driven

--- Formação Angular/Boas praticas/angular_formularios/gatitobook/src/app/home/login/login.component.ts
+++ Formação Angular/Boas praticas/angular_formularios/gatitobook/src/app/home/login/login.component.ts
@@ -3,13 +3,20 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
+  usuario = '';
+  senha = '';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  login() {
+    console.table({
+      usuario: this.usuario,
+      senha: this.senha,
+    });
   }
-
 }
