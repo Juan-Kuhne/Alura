eac660b16e0878f13d150b42eec97387be267ebf
Juan_Kuhne <juan.kuhne@certisign.com.br>
2022-09-15 15:07:14 -0300

    Formulário Reativo

--- Formação Angular/Boas praticas/angular_formularios/gatitobook/src/app/home/novo-usuario/novo-usuario.component.ts
+++ Formação Angular/Boas praticas/angular_formularios/gatitobook/src/app/home/novo-usuario/novo-usuario.component.ts
@@ -1,15 +1,32 @@
+import { NovousuarioService } from './novousuario.service';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Novousuario } from './novousuario';
 
 @Component({
   selector: 'app-novo-usuario',
   templateUrl: './novo-usuario.component.html',
-  styleUrls: ['./novo-usuario.component.css']
+  styleUrls: ['./novo-usuario.component.css'],
 })
 export class NovoUsuarioComponent implements OnInit {
+  novoUsuarioForm!: FormGroup;
 
-  constructor() { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private novousuarioService: NovousuarioService
+  ) {}
 
   ngOnInit(): void {
+    this.novoUsuarioForm = this.formBuilder.group({
+      email: [''],
+      fullName: [''],
+      userName: [''],
+      password: [''],
+    });
   }
 
+  cadastrar() {
+    const novoUsuario = this.novoUsuarioForm.getRawValue() as Novousuario;
+    console.log(novoUsuario);
+  }
 }
