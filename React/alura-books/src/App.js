b89f88e0b6fcccbb88e9a223870c64d2af39a044
juan-kuhne <juankuhne@hotmail.com>
2023-06-07 18:06:17 -0300

    Usando Loops

--- React/alura-books/src/App.js
+++ React/alura-books/src/App.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react';
 import './App.css';
 import Logo from './componentes/Logo';
+import perfil from './imagens/perfil.svg';
+import sacola from './imagens/sacola.svg';
+
+const textoOpcoes = ['CATEGORIAS', 'FAVORITOS', 'MINHA ESTANTE'];
+const icones = [perfil, sacola];
 
 class App extends Component {
   render() {
@@ -8,6 +13,17 @@ class App extends Component {
       <div className='App'>
         <header className='App-header'>
           <Logo></Logo>
+          <ul className='opcoes'>
+            { textoOpcoes.map((opcao) => (
+              <li className='opcao'><p>{opcao}</p></li>
+            )) }
+          </ul>
+
+          <ul className='icones'>
+            { icones.map((icone) => (
+              <li className='icone'><img src={icone}></img></li>
+            )) }
+          </ul>
         </header>
       </div>
     );
