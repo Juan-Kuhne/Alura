9158b517278b3bd8c4f856eb2f153499d394c4e3
juan-kuhne <juankuhne@hotmail.com>
2023-06-12 14:02:35 -0300

    Global Style

--- React/alura-books/src/index.js
+++ React/alura-books/src/index.js
@@ -1,9 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import './index.css';
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+    font-family: sans-serif;
+  }
+
+  li {
+    list-style: none;
+  }
+`
 
 ReactDOM.render(
-  <App />,
+  <div>
+    <GlobalStyle/>
+    <App />
+  </div>,
   document.getElementById('root')
 );
