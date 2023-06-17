3ca954780fba1eecfbdd609420fa4c4c143423c9
juan-kuhne <juankuhne@hotmail.com>
2023-06-16 22:16:17 -0300

    Estilizando com props

--- React/alura-books/src/componentes/UltimosLancamentos/index.js
+++ React/alura-books/src/componentes/UltimosLancamentos/index.js
@@ -1,22 +1,13 @@
 import React from "react"
 import { livros } from "./dadosUltimosLancamentos"
 import styled from "styled-components"
+import { Titulo } from "../Titulo"
 
 const UltimosLancamentosContainer = styled.section`
    background-color: #EBECEE;
    padding-bottom: 20px;
    display: flex;
    flex-direction: column;
-`
-
-const Titulo = styled.h2`
-   width: 100%;
-   padding: 30px 0;
-   background-color: #FFF;
-   color: #EB9B00;
-   font-size: 36px;
-   text-align: center;
-   margin: 0;
 `
 
 const NovosLivrosContainer = styled.div`
@@ -34,7 +25,7 @@ const NovosLivrosContainer = styled.div`
 function UltimosLancamentos() {
    return (
       <UltimosLancamentosContainer>
-         <Titulo>ULTIMOS LANÇAMENTOS</Titulo>
+         <Titulo cor="#EB9B00" tamanhoFonte="36px">ULTIMOS LANÇAMENTOS</Titulo>
          <NovosLivrosContainer>
             {livros.map( livro => (
                <img src={livro.src} alt={livro.nome} />
