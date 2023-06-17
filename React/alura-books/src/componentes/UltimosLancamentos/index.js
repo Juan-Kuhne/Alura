3b7f6d21163355253a2567aca1d77f553bc0edb5
juan-kuhne <juankuhne@hotmail.com>
2023-06-16 22:34:37 -0300

    Card reutilizável com props

--- React/alura-books/src/componentes/UltimosLancamentos/index.js
+++ React/alura-books/src/componentes/UltimosLancamentos/index.js
@@ -2,6 +2,8 @@ import React from "react"
 import { livros } from "./dadosUltimosLancamentos"
 import styled from "styled-components"
 import { Titulo } from "../Titulo"
+import CardRecomenda from "../CardRecomenda"
+import livro2 from '../../imagens/livro2.png'
 
 const UltimosLancamentosContainer = styled.section`
    background-color: #EBECEE;
@@ -31,6 +33,12 @@ function UltimosLancamentos() {
                <img src={livro.src} alt={livro.nome} />
             ))}
          </NovosLivrosContainer>
+         <CardRecomenda 
+            titulo="Talvez você se enteresse por ..."
+            subtitulo="Angular 11"
+            descricao="Construindo uma aplicação integrada com a plataforma Google."
+            img={livro2}
+         />
       </UltimosLancamentosContainer>
    )
 }
